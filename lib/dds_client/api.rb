3059b44f9328f5a613226f744cffdc351d7be47b
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-01-26 08:42:48 +0100

    Modify SSL verification logic
    Since passing custom server certificate can be quite cumbersome, we
    replaced the option to specify custom SSL certificate with the simple
    flags that toggles the certification on or off.
    
    By default, verification is enabled and can be disabled by setting
    DDS_VERIFY_SSL environment variable to false (any other value will
    still activate the validation).

--- lib/dds_client/api.rb
+++ lib/dds_client/api.rb
@@ -6,32 +6,37 @@ require "dds_client/exceptions"
 module DdsClient
   class Api
     def self.create_from_env
-      new(ENV.fetch("DDS_URL"), ENV.fetch("DDS_USERNAME"),
-          ENV.fetch("DDS_PASSWORD"), ENV["DDS_CA_FILE"])
+      puts(ENV.fetch("DDS_VERIFY_SSL", "true"))
+      new(
+        ENV.fetch("DDS_URL"),
+        ENV.fetch("DDS_USERNAME"),
+        ENV.fetch("DDS_PASSWORD"),
+        ENV.fetch("DDS_VERIFY_SSL", "true") != "false"
+      )
     rescue KeyError => e
       raise DdsClient::ConfigError, "Set environment vars (#{e})"
     end
 
-    def initialize(url, username, password, ca_file)
+    def initialize(url, username, password, verify_ssl)
       url = url.chomp("/")
-      token = authenticate(url, username, password, ca_file)
+      token = authenticate(url, username, password, verify_ssl)
 
       params = {
         headers: {
           "Content-Type" => "application/json",
           "Authorization" => "Token #{token}"
         },
-        ssl_ca_file: ca_file
+        ssl_verify_peer: verify_ssl
       }
       @connection = Excon.new(url, params)
     end
 
-    def authenticate(url, username, password, ca_file)
+    def authenticate(url, username, password, verify_ssl)
       r = Excon.post(
         "#{url}/auth/get-token",
         headers: { "Content-Type" => "application/json" },
         body: { "username" => username, "password" => password }.to_json,
-        ssl_ca_file: ca_file
+        ssl_verify_peer: verify_ssl
       )
       raise DdsClient::AuthError, "Invalid credentials" if r.status != 200
       JSON.parse(r.body)["token"]
